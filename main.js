19ea965057b6b5654346c0027f8ef5412892827f
bernardojwassen <bernardojwassen@gmail.com>
2024-09-24 08:46:30 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-const btsToggleNav = document.querySelectorAll('button.toggle-nav')
-const nv = document.querySelector('nav')
+async function listaPersonagens() {
+  const template = document.querySelector('#modelo-personagem')
+  const request = await fetch('personagens.json')
+  const data = await request.json()
 
-btsToggleNav.forEach(function(bt){
-  bt.addEventListener('click', function(){
-    nv.classList.toggle('open')
+  data.forEach(personagem => {
+    const clone = template.content.cloneNode(true)
+    const nome = clone.querySelector('h2')
+    const hostilidade = clone.querySelector('p')
+    nome.textContent = personagem.nome
+    hostilidade.textContent = personagem.hostilidade
+    template.parentNode.appendChild(clone)
   })
-})
+}
+
+listaPersonagens()
